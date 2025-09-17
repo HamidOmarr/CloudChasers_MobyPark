ac3663e6dfca1b0bd6729d90699370740ee18880
Faye <fayecels@gmail.com>
2025-09-17 14:17:00 +0200

    Add ParkingSessionController and add ParkingSession to DataService

---
+++ MobyPark/Controllers/ParkingSessionController.cs
@@ -0,0 +1,6 @@
+namespace MobyPark.Controllers;
+
+public class ParkingSessionController
+{
+    // TODO: Add API calls if needed, or remove
+}
--- MobyPark/Models/DataService/DataService.cs
+++ MobyPark/Models/DataService/DataService.cs
@@ -5,14 +5,16 @@ namespace MobyPark.Models.DataService;
 public class DataService : IDataService
 {
     public IParkingLotAccess ParkingLots { get; }
+    public IParkingSessionAccess ParkingSessions { get; }
     public IPaymentAccess Payments { get; }
     public IReservationAccess Reservations { get; }
     public IUserAccess Users { get; }
     public IVehicleAccess Vehicles { get; }
 
-    public DataService(IParkingLotAccess parkingLots, IPaymentAccess payments, IReservationAccess reservations, IUserAccess users, IVehicleAccess vehicles)
+    public DataService(IParkingLotAccess parkingLots, IParkingSessionAccess parkingSessions, IPaymentAccess payments, IReservationAccess reservations, IUserAccess users, IVehicleAccess vehicles)
     {
         ParkingLots = parkingLots;
+        ParkingSessions = parkingSessions;
         Payments = payments;
         Reservations = reservations;
         Users = users;
