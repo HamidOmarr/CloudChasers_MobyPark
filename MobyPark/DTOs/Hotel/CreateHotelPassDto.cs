540a51551a09f9fa076695e3ac4891aad19ce79f
amari999 <kennashaendir@gmail.com>
2025-11-22 01:38:52 +0100

    HotelPass Dto and Model added

---
+++ MobyPark/DTOs/Hotel/CreateHotelPassDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobyPark.DTOs.Hotel;
+
+public class CreateHotelPassDto
+{
+    [Required]
+    public string LicensePlate { get; set; }
+    [Required]
+    public int ParkingLotId { get; set; }
+    [Required]
+    public DateTime Start { get; set; }
+    [Required]
+    public DateTime End { get; set; }
+}
+
+public class ReadHotelPassDto
+{
+    [Required]
+    public long Id { get; set; }
+    [Required]
+    public string LicensePlate { get; set; }
+    [Required]
+    public int ParkingLotId { get; set; }
+    [Required]
+    public DateTime Start { get; set; }
+    [Required]
+    public DateTime End { get; set; }
+}
+
+public class PatchHotelPassDto
+{
+    public string? LicensePlate { get; set; }
+    public DateTime? Start { get; set; }
+    public DateTime? End { get; set; }
+}
---
+++ MobyPark/Models/HotelPassModel.cs
@@ -0,0 +1,12 @@
+﻿using MobyPark.Models.Repositories.Interfaces;
+
+namespace MobyPark.Models;
+
+public class HotelPassModel : IHasLongId
+{ 
+    public long Id { get; }
+    public int ParkingLotId { get; set; }
+    public ParkingLotModel ParkingLot { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
