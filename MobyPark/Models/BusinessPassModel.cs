badc7ffe620822f64d644c937a3211ddf517fc6f
amari999 <kennashaendir@gmail.com>
2025-12-01 14:13:28 +0100

    vragen van po beantwoord

--- MobyPark/Models/BusinessPassModel.cs
+++ MobyPark/Models/BusinessPassModel.cs
@@ -7,13 +7,24 @@ public class BusinessPassModel : IHasLongId
 {
     public long Id { get; set; }
     // even vragen of dit erbij moet
+    // ja
     public string BusinessName { get; set; }
     public long ParkingLotId { get; set; }
     [ForeignKey(nameof(ParkingLotId))]
     public ParkingLotModel ParkingLot { get; set; }
     
+    // voor businesses:
     // Bij hoeveel parkeerplaatsen moeten de auto's kunnen parkeren?
+    // kan bij meerdere
     // Is de pass voor een enkele parkeersplaats of moeten auto's met een pass op alle parkeerplaatsen kunnen parkeren?
     // Wordt er 1 vast bedrag betaald per auto of moeten de parkeertijden ook opgeslagen worden?
+    // tarieven worden gewoon berekend per sessie.
     
+    /*
+     * Voor hotels: hoe moeten de hoteleigenaren worden geverifieerd wanneer zij een account maken?
+     * user wordt gemaakt voor mobypark
+     * Maakt de admin een account voor hen?
+     * Kan een hoteleigenaar alleen voor hun eigen parkeerplaats een hotelpass aanmaken?
+     * ja
+     */
 }
