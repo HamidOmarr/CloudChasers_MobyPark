dc7c13ca4307a1981b2d9e051103a5b54c08666d
Faye <fayecels@gmail.com>
2026-01-22 09:51:08 +0100

    Adding delay to backup creation
    Turning off making backup temporarily

--- generate_dump.sh
+++ generate_dump.sh
@@ -7,12 +7,20 @@ export $(grep -v '^#' .env | xargs)
 # Make sure DB container is running
 docker compose up -d mobypark-db
 
-# Dump DB as Postgres user
-docker compose exec -u postgres mobypark-db \
-  pg_dump -U "$POSTGRES_USER" -d "$POSTGRES_DB" -Fc -f /var/lib/postgresql/data/mobypark.dump
+echo "Waiting for database to initialize..."
+until docker compose exec -u postgres mobypark-db pg_isready -U "$POSTGRES_USER"; do
+  echo "Database is starting up... waiting 2 seconds"
+  sleep 2
+done
 
-# Copy dump to server home directory
-CONTAINER=$(docker compose ps -q mobypark-db)
-docker cp "$CONTAINER":/var/lib/postgresql/data/mobypark.dump ~/CloudChasers_MobyPark/mobypark.dump
+echo "Database is ready. Starting backup..."
+
+# Dump DB as Postgres user
+#docker compose exec -u postgres mobypark-db \
+#  pg_dump -U "$POSTGRES_USER" -d "$POSTGRES_DB" -Fc -f /var/lib/postgresql/data/mobypark.dump
+#
+## Copy dump to server home directory
+#CONTAINER=$(docker compose ps -q mobypark-db)
+#docker cp "$CONTAINER":/var/lib/postgresql/data/mobypark.dump ~/CloudChasers_MobyPark/mobypark.dump
 
 echo "Database dump available at ~/CloudChasers_MobyPark/mobypark.dump"
